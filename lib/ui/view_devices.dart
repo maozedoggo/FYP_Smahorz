0dfe0e47e0bc16f5c91b6d7e8d958d2d5f630862
DanishSalute <danishguy91@gmail.com>
2025-08-03 18:40:49 +0800

    Adjust Homepage

---
+++ lib/ui/view_devices.dart
@@ -0,0 +1,76 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ViewDevices extends StatelessWidget {
+  final String deviceType;
+  final String devicePart;
+  final String iconPath;
+  final bool status;
+  void Function(bool)? onChanged;
+
+  ViewDevices({
+    super.key,
+    required this.deviceType,
+    required this.devicePart,
+    required this.iconPath,
+    required this.status,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(
+            vertical: 15,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+
+              //Type of Devices
+              Text(deviceType),
+
+              //Icons
+              Image.asset(
+                iconPath,
+                height: 65,
+              ),
+
+              //Part of Devices
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                          devicePart,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                  
+                    CupertinoSwitch(
+                      value: status, 
+                      onChanged: onChanged
+                    
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_horizon_home/ui/view_devices.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Padding's variables
   final double horizontalPadding = 40.0;
   final double verticalPadding = 20.0;
+
+  // List of Smart Devices
+  List smartDevices = [
+    ["Parcel Box", "Outside", "lib/icons/door-open.png", true],
+    ["Parcel Box", "Inside", "lib/icons/door-open.png", true],
+    ["Cloth Hanger", "", "lib/icons/drying-rack.png", true],
+  ];
+
+  //Smart Device Switch
+  void powerSwitchChanged(bool value, int index) {
+    setState(() {
+      smartDevices[index][3] = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +41,16 @@ class _HomePageState extends State<HomePage> {
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.menu, size: 50, color: Colors.grey[800]),
-
-                  Icon(Icons.person, size: 50, color: Colors.grey[800]),
-                ],
+                children: [Icon(Icons.menu, size: 35, color: Colors.grey[800])],
               ),
             ),
 
             const SizedBox(height: 20),
 
+
+            //Welcome bar and name of user
             Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding
-                ),
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -49,6 +61,38 @@ class _HomePageState extends State<HomePage> {
 
                   Text("Name", style: TextStyle(fontSize: 30)),
                 ],
+              ),
+            ),
+
+            // Divider, decoration
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 40.0),
+              child: Divider(
+                thickness: 4,
+                color: Colors.grey[400],
+                ),
+              ),
+
+            // Smart devices list
+            Expanded(
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.all(25),
+                itemCount: smartDevices.length,
+
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (context, index) {
+                  return ViewDevices(
+                    deviceType: smartDevices[index][0],
+                    devicePart: smartDevices[index][1],
+                    iconPath: smartDevices[index][2],
+                    status: smartDevices[index][3],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },
               ),
             ),
           ],
