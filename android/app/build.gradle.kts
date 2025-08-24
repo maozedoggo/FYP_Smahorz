e8c86d2838d67bd754dd7a5c538394d7e0770689
DanishSalute <danishguy91@gmail.com>
2025-08-24 22:08:01 +0800

    Fix Gradle & Align Version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,35 +1,44 @@
+//
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.smart_horizon_home"
-    compileSdk = 35
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.smart_horizon_home"
-        minSdk = 23            // Firebase Auth requires min 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-
-        compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
-        }
-
-    kotlinOptions {
-        jvmTarget = "11"
     }
 }
 
@@ -41,7 +50,6 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3") // << MUST be here
 }
 
-// Apply Flutter plugin
 flutter {
     source = "../.."
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,11 @@
-// android/build.gradle.kts
-
-buildscript {
+buildscript{
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath("com.android.tools.build:gradle:8.2.1") // Android Gradle Plugin
+        classpath("com.android.tools.build:gradle:8.4.0") // Android Gradle Plugin
         classpath("com.google.gms:google-services:4.4.2") // Google Services plugin for Firebase
     }
 }
@@ -17,6 +15,17 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 // Clean task for project
