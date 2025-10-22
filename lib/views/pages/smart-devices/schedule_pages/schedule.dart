8f07e623c2d07b9053ef0e687997f281fe10bc85
DanishSalute <danishguy91@gmail.com>
2025-10-22 17:04:24 +0800

    Change schedule theme

--- lib/views/pages/smart-devices/schedule_pages/schedule.dart
+++ lib/views/pages/smart-devices/schedule_pages/schedule.dart
@@ -21,7 +21,7 @@ class _SchedulePageState extends State<SchedulePage> {
           children: [
             // Back button
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
               child: Align(
                 alignment: Alignment.topLeft,
                 child: IconButton(
@@ -34,11 +34,49 @@ class _SchedulePageState extends State<SchedulePage> {
               ),
             ),
 
+            Align(
+              child: Text(
+                widget.deviceName,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
             // Calendar
             TableCalendar(
               focusedDay: DateTime.now(),
-              firstDay: DateTime.now(),
+              firstDay: DateTime.utc(2020, 1, 1),
               lastDay: DateTime.utc(2050, 12, 31),
+              calendarFormat:
+                  CalendarFormat.month,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              daysOfWeekVisible: true,
+
+              headerStyle: const HeaderStyle(
+                formatButtonVisible:
+                    false,
+                titleCentered: true,
+                titleTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              calendarStyle: const CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.green,
+                  shape: BoxShape.circle,
+                ),
+                weekendTextStyle: TextStyle(color: Colors.red),
+                defaultTextStyle: TextStyle(color: Colors.black),
+              ),
             ),
           ],
         ),
