04bc9489de556af7780a69c4ce99b4da774e4019
DanishSalute <danishguy91@gmail.com>
2025-10-22 17:52:41 +0800

    add-style-schedule

--- lib/views/pages/smart-devices/schedule_pages/schedule.dart
+++ lib/views/pages/smart-devices/schedule_pages/schedule.dart
@@ -11,6 +11,13 @@ class SchedulePage extends StatefulWidget {
 }
 
 class _SchedulePageState extends State<SchedulePage> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,35 +54,56 @@ class _SchedulePageState extends State<SchedulePage> {
 
             // Calendar
             TableCalendar(
-              focusedDay: DateTime.now(),
-              firstDay: DateTime.utc(2020, 1, 1),
+              // Calendar Settings
+              focusedDay: today,
+              firstDay: DateTime.utc(2025, 10, 1),
               lastDay: DateTime.utc(2050, 12, 31),
-              calendarFormat:
-                  CalendarFormat.month,
+              calendarFormat: CalendarFormat.month,
               startingDayOfWeek: StartingDayOfWeek.monday,
               daysOfWeekVisible: true,
 
+              // Disable past days
+              enabledDayPredicate: (day) {
+                return !day.isBefore(
+                  DateTime(
+                    DateTime.now().year,
+                    DateTime.now().month,
+                    DateTime.now().day,
+                  ),
+                );
+              },
+
+              // Calendar Styles
               headerStyle: const HeaderStyle(
-                formatButtonVisible:
-                    false,
+                formatButtonVisible: false,
                 titleCentered: true,
                 titleTextStyle: TextStyle(
                   fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.normal,
                 ),
               ),
 
-              calendarStyle: const CalendarStyle(
+              // Selected Day Styles
+              availableGestures: AvailableGestures.all,
+              rowHeight: 60,
+              onDaySelected: _onDaySelected,
+              selectedDayPredicate: (day) => isSameDay(today, day),
+
+              // Another Calendar Styles
+              calendarStyle: CalendarStyle(
                 todayDecoration: BoxDecoration(
-                  color: Colors.blueAccent,
+                  color: Colors.lightBlue[100],
                   shape: BoxShape.circle,
                 ),
                 selectedDecoration: BoxDecoration(
-                  color: Colors.green,
+                  color: Colors.blueAccent,
                   shape: BoxShape.circle,
                 ),
                 weekendTextStyle: TextStyle(color: Colors.red),
                 defaultTextStyle: TextStyle(color: Colors.black),
+                disabledTextStyle: TextStyle(
+                  color: Colors.grey,
+                ),
               ),
             ),
           ],
